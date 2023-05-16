34ead72194b803bbc48cb49012a1e96aba562a6d
skelrath <zzzkelrath@yandex.ru>
2023-05-16 19:28:27 +0700

    paste together 2 project

--- app/src/main/java/com/example/testfinalproject/MainActivity.java
+++ app/src/main/java/com/example/testfinalproject/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -143,6 +144,9 @@ public class MainActivity extends AppCompatActivity {
         editTextСategory = constraintLayout.findViewById(R.id.editTextСategory);
         editTextResult = constraintLayout.findViewById(R.id.editTextResult);
         saveEventButton = constraintLayout.findViewById(R.id.saveEventButton);
+
+
+
         saveEventButton.setOnClickListener( v-> {
 
 
